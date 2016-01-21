8f7090ba1d2002320d55ca4b2cd86fae15ad04c1
srunnikr <srunnikr@eng.ucsd.edu>
2016-01-21 13:17:44 -0800

    Updated arduino

--- arduino/ArmMotorControl.ino/ArmMotorControl.ino.ino
+++ arduino/ArmMotorControl.ino/ArmMotorControl.ino.ino
@@ -1,161 +1,186 @@
-/* 
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
-
-For use with the Adafruit Motor Shield v2 
----->	http://www.adafruit.com/products/1438
-*/
-
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-
-// Select which 'port' M1, M2, M3 or M4. In this case, M1
-Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
-Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
-Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
-
-// Analog Sensors
 int sensor1 = A0;
 int sensor2 = A1;
 int sensor3 = A2;
 int sensor4 = A3;
 int sensor5 = A4;
 
-
-// You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *myMotor1 = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 
 void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test! \r\n");
-
-  AFMS.begin();  // create with the default frequency 1.6KHz
-  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  Serial.print("After Begin \r\n");
- 
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  AFMS.begin();
 }
 
 void loop() {
-  uint8_t i;
-  char motorNumber;
+  // put your main code here, to run repeatedly:
+  char code;
   int sensorid;
   int angle;
+  int dir;
 
-  printf( "Engage Motor number: \r\n");
-  while(!Serial.available()){
-    
+  while(1) {
+    if(Serial.available()) {
+      code = Serial.read();
+      if (code == 's') {
+        //Serial.println("Received s!");
+        String sensorReadings = "";
+        sensorReadings += String(getSensor1Value());
+        sensorReadings += " ";
+        sensorReadings += String(getSensor2Value());
+        sensorReadings += " ";
+        sensorReadings += String(getSensor3Value());
+        sensorReadings += " ";
+        sensorReadings += String(getSensor4Value());
+        sensorReadings += " ";
+        sensorReadings += String(getSensor5Value());
+        Serial.println(sensorReadings);
+      } else if (code == 'm') {
+        long temp = Serial.parseInt();
+        sensorid = (int) temp / 10000;
+        temp = temp % 10000;
+        // Dir -> 0 => backward, 1 => forward
+        dir = temp / 1000;
+        angle = temp % 1000;
+        /*Serial.print("Motor id: ");
+        Serial.print(sensorid);
+        Serial.print(" Direction bit : ");
+        Serial.print(dir);
+        Serial.print(" Angle : ");
+        Serial.print(angle);
+        Serial.println(); */
+
+        //Serial.println("trying to move the motor");
+        switch (sensorid) {
+          case 1:
+            moveMotor1(dir,angle);
+            break;
+          case 2:
+            moveMotor2(dir,angle);
+            break;
+          case 3:
+            moveMotor3(dir,angle);
+            break;
+          case 4:
+            moveMotor4(dir,angle);
+            break;
+          default:
+            //Serial.println("Got a wrong motor id");
+            break;
+        }
+      }
+    }
   }
+}
+
+void calibrateSensor1(int duration) {
+  Serial.println("Initial Sensor Reading : ");
+  Serial.print(getSensor1Value());
   
-  motorNumber = Serial.read();
+  myMotor1->run(FORWARD);
+  myMotor1->setSpeed(50);
+  delay(duration);
+  myMotor1->setSpeed(0);
 
-  if(motorNumber == 'a'){
-    moveMotor(myMotor1);
+  Serial.println("Final Sensor Reading : ");
+  Serial.print(getSensor1Value());
+  
+  myMotor1->run(BACKWARD);
+  myMotor1->setSpeed(50);
+  delay(duration);
+  myMotor1->setSpeed(0);
+}
+
+void moveMotor1(int dir,int angle) {
+  int initialSensorVal = getSensor1Value();
+  // Current calibration -> 1 degree => 5 points in sensor
+  int changeRequired = 5 * angle;
+  
+  if(dir == 0)
+    myMotor1->run(BACKWARD);
+  else if(dir ==1)
+    myMotor1->run(FORWARD);
+    
+  myMotor1->setSpeed(50);
+  int currentSensorVal = getSensor1Value();
+  while(abs(currentSensorVal - initialSensorVal) < changeRequired) {
+    currentSensorVal = getSensor1Value();
   }
-  else if(motorNumber == 'b'){
-    moveMotor(myMotor2);
+  myMotor1->setSpeed(0);
+}
+
+void moveMotor2(int dir,int angle) {
+  if(dir == 0)
+    myMotor2->run(BACKWARD);
+  else
+    myMotor2->run(FORWARD);
+    
+  for (int i=0; i<180; i++) {
+    myMotor2->setSpeed(i);  
+    delay(10);
   }
-  else if(motorNumber == 'c'){
-    moveMotor(myMotor3);
+  for (int i=180; i!=0; i--) {
+    myMotor2->setSpeed(i);  
+    delay(10);
   }
-  else if(motorNumber == 'd'){
-    moveMotor(myMotor4);
+}
+
+void moveMotor3(int dir,int angle) {
+  if(dir == 0)
+    myMotor3->run(BACKWARD);
+  else
+    myMotor3->run(FORWARD);
+    
+  for (int i=0; i<180; i++) {
+    myMotor3->setSpeed(i);  
+    delay(10);
   }
-  else if(motorNumber == 's'){
-       Serial.println("Received s!");
-       String sensorReadings = "";
-       sensorReadings += String(getSensor1Value());
-       sensorReadings += " ";
-       sensorReadings += String(getSensor2Value());
-       sensorReadings += " ";
-       sensorReadings += String(getSensor3Value());
-       sensorReadings += " ";
-       sensorReadings += String(getSensor4Value());
-       sensorReadings += " ";
-       sensorReadings += String(getSensor5Value());
-       Serial.println(sensorReadings);
-   } 
-   else if (motorNumber == 'm') {
-       long temp = Serial.parseInt();
-       sensorid = (int) temp / 1000;
-       angle = temp % 1000;
-       Serial.print("Motor id: ");
-       Serial.print(sensorid);
-       Serial.print(" Angle : ");
-       Serial.print(angle);
-       Serial.println();
-   }
+  for (int i=180; i!=0; i--) {
+    myMotor3->setSpeed(i);  
+    delay(10);
+  }
+}
+
+void moveMotor4(int dir,int angle) {
+  if(dir == 0)
+    myMotor4->run(BACKWARD);
+  else
+    myMotor4->run(FORWARD);
+    
+  for (int i=0; i<180; i++) {
+    myMotor4->setSpeed(i);  
+    delay(10);
+  }
+  for (int i=180; i!=0; i--) {
+    myMotor4->setSpeed(i);  
+    delay(10);
+  }
 }
 
 int getSensor1Value() {
- return analogRead(sensor1);
+  return analogRead(sensor1);
 }
 
 int getSensor2Value() {
- return analogRead(sensor2);
+  return analogRead(sensor2);
 }
 
 int getSensor3Value() {
- return analogRead(sensor3);
+  return analogRead(sensor3);
 }
 
 int getSensor4Value() {
- return analogRead(sensor4);
+  return analogRead(sensor4);
 }
 
 int getSensor5Value() {
- return analogRead(sensor5);
+  return analogRead(sensor5);
 }
-
-void moveMotor(Adafruit_DCMotor *myMotor){
-  uint8_t i;
-  char dir;
-
-  Serial.print("Direction: a: forward, b: backward \r\n");
-  while(!Serial.available()){
-    
-  }
-    
-  dir = Serial.read();
-
-  Serial.print("direction read: \r\n" );
-  Serial.print(dir);
-
-  if(dir == 'a'){
-    myMotor->run(FORWARD);
-    for (i=0; i<180; i++) {
-      myMotor->setSpeed(i);  
-      delay(10);
-    }
-    for (i=180; i!=0; i--) {
-      myMotor->setSpeed(i);  
-      delay(10);
-    }
-    
-  }
-  
-  else if(dir == 'b'){
-    myMotor->run(BACKWARD);
-    for (i=0; i<180; i++) {
-      myMotor->setSpeed(i);  
-      delay(10);
-    }
-    for (i=180; i!=0; i--) {
-      myMotor->setSpeed(i);  
-      delay(10);
-    }
-  }
-  else{}
-
-  myMotor->run(RELEASE);
-  delay(1000);
-}
-
