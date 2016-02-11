da49d566fbd647325ae8059cb136e12325aac4c7
Sean Grady <vedicmonk@gmail.com>
2016-02-11 12:01:09 -0800

    Added circle detect with cv2.HoughCircles, haven't tested

--- scripts/better_object_detection.py
+++ scripts/better_object_detection.py
@@ -11,10 +11,13 @@ class BallDetector():
         closeKernSize = 9
         self.close_kernel = np.ones((closeKernSize,closeKernSize), np.uint8)
         self.open_kernel = np.ones((openKernSize,openKernSize), np.uint8)
+        self.hough_accumulator = 1.5
+        self.hough_min_dist = 80
         self.camera = cv2.VideoCapture(0)
 
     def create_hsv_mask(self, rgb_image):
-        #resize frame, blur it, and convert to HSV colorspace
+        #blur frame and convert to HSV colorspace
+        #may end up resizing frame if need more FPS on odroid
         #frame = imutils.resize(rgb_image, width=600)
         blurred = cv2.GaussianBlur(rgb_image, (self.blur_size, self.blur_size), 0)
         hsv = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
@@ -26,16 +29,51 @@ class BallDetector():
         return mask
 
     def mask_image(self, image, mask):
+        #There's probably a cleaner way to do this but it's fast
         masked_image = cv2.bitwise_and(image, image, mask = mask)
         return masked_image
 
-    def test_mask(self):
+    def find_circles(self, image):
+        #Assumes unmasked BGR image
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        circles = cv2.HoughCircles(
+                gray_image,
+                cv2.cv.CV_HOUGH_GRADIENT,
+                self.hough_accumulator,
+                self.hough_min_dist
+        )
+        return circles
+
+    def draw_circles(self, image, circles):
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            for x, y, r in circles:
+                cv2.circle(image, (x, y), r, (0, 255, 0), 4)
+                cv2.rectangle(
+                        image,
+                        (x - 5, y - 5),
+                        (x + 5, y + 5),
+                        (0, 128, 255),
+                        -1
+                )
+        return image
+
+    def threshold_color(self, image):
+        mask = self.create_hsv_mask(image)
+        masked_image = self.mask_image(image, mask)
+        return masked_image
+
+    def hough_circles(self, image):
+        circles = self.find_circles(image)
+        circled_image = self.draw_circles(image, circles)
+        return circled_image
+
+    def test_cv_func(self, function):
         while(1):
             ret, image = self.camera.read()
             if ret:
-                mask = self.create_hsv_mask(image)
-                masked_image = self.mask_image(image, mask)
-                cv2.imshow("image", masked_image)
+                disp_image = function(image)
+                cv2.imshow("image", disp_image)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
         self.camera.release()
@@ -43,4 +81,4 @@ class BallDetector():
 
 if __name__ == "__main__":
     bd = BallDetector()
-    bd.test_mask()
+    bd.test_cv_func(bd.threshold_color)
